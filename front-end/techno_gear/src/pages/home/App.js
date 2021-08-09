bff5f7f10cf8a65335683366f5e051b2fa2621aa
Alan-Santos07 <alancostasantos07@gmail.com>
2021-08-09 09:31:25 -0300

    layout alterado | pendente metodo post no front

--- front-end/techno_gear/src/pages/home/App.js
+++ front-end/techno_gear/src/pages/home/App.js
@@ -7,23 +7,83 @@ export default class Home extends Component{
     super(props)
     this.state = {
         listaEquipamentos : [],
-        
+        listaSalas : [],
+        listaTiposEquipamentos : [],
+        AndarSala : '0',
+        TipoEquipamentoNovo : 0,
+        NomeSalaNova : '',
+        MetragemSalaNova : '',
+        SituacaoNova : 5,
+        NumeroSerieNovo : '',
+        NumeroPatrimonioNovo : '',
+        DescricaoNova : '',
+        idEquipamentoSelecionado : 0,
     }
   }
 
-  // buscarEquipamentos = () => {
+  buscarEquipamentos = () => {
 
-  //   axios('http://localhost:5000/api/')
+    axios('http://localhost:5000/api/equipamentos')
 
-  //   .then(resposta => {
-  //       if (resposta.status === 200) {
-  //           this.setState({ listaEquipamentos : resposta.data })
-  //           console.log(this.state.listaEquipamentos)
-  //       }
-  //     })
+    .then(resposta => {
+        if (resposta.status === 200) {
+            this.setState({ listaEquipamentos : resposta.data })
+            console.log(this.state.listaEquipamentos)
+        }
+      })
 
-  //   console.log("teste")
-  // }
+    console.log("teste")
+  }
+
+  buscarTiposEquipamentos = () => {
+
+    axios('http://localhost:5000/api/tiposEquipamentos')
+
+    .then(resposta => {
+        if (resposta.status === 200) {
+            this.setState({ listaTiposEquipamentos : resposta.data })
+            console.log(this.state.listaTiposEquipamentos)
+        }
+      })
+
+    console.log("teste")
+  }
+
+  buscarSalas = () => {
+
+    axios('http://localhost:5000/api/salas')
+
+    .then(resposta => {
+        if (resposta.status === 200) {
+            this.setState({ listaSalas : resposta.data })
+            console.log(this.state.listaSalas)
+        }
+      })
+
+    console.log("teste")
+  }
+
+  excluirEquipamento = (equipamento) => {
+
+    this.setState({
+
+      idEquipamentoSelecionado : equipamento.idEquipamento
+
+    })
+
+    axios.delete('http://localhost:5000/api/equipamentos/' + this.state.idEquipamentoSelecionado)
+
+    .then(resposta => {
+      if (resposta.status === 204) {
+        console.log('deletou tudo rapaiz')
+      }
+    })
+
+    .catch(erro => console.log(erro))
+
+    .then(this.buscarEquipamentos)
+
+  }
 
   // deslogar() {
   //   localStorage.clear();
@@ -32,6 +92,8 @@ export default class Home extends Component{
 
   componentDidMount(){
     this.buscarEquipamentos()
+    this.buscarTiposEquipamentos()
+    this.buscarSalas()
   }
 
   atualizaStateCampo = (campo) => {
@@ -47,6 +109,7 @@ export default class Home extends Component{
                 <tr>
                     <th>Tipo de Equipamento</th>
                     <th>Situação</th>
+                    {/* <th>Sala</th> */}
                     <th>Número de Série</th>
                     <th>Número de Patrimônio</th>
                     <th>Descrição</th>
@@ -55,15 +118,16 @@ export default class Home extends Component{
             </thead>
             <tbody>
               {
-                this.state.listaEquipamentos.map((consulta) => {
+                this.state.listaEquipamentos.map((equipamentos) => {
                   return(
-                    <tr key={equipamentos.idEquipamentos}>
-                      <td>{equipamentos.tipoEquipamento}</td>
+                    <tr key={equipamentos.idEquipamento}>
+                      <td>{equipamentos.idTipoEquipamentoNavigation.nomeEquipamento}</td>
                       <td>{equipamentos.situacao}</td>
+                      {/* <td>{equipamentos.}</td> */}
                       <td>{equipamentos.numeroSerie}</td>
                       <td>{equipamentos.numeroPatrimonio}</td>
                       <td>{equipamentos.descricao}</td>
-                      <td><button>Deletar</button></td>
+                      <td><button onClick={() => this.excluirEquipamento}>Deletar</button></td>
                     </tr>
                   )
                 })
@@ -71,18 +135,33 @@ export default class Home extends Component{
             </tbody>
           </table>
           <div>
+            <h3>Cadastrar Sala</h3>
             <form>
               <div>
+      
+                <select
+                  name='AndarSala'
+                  id='andar'
+                  onChange={this.atualizaStateCampo}
+                  value={this.state.AndarSala} >
 
-                <select>
-
+                    <option value="0" disabled > Selecione o Andar </option>
+                    {
+                      this.state.listaSalas.map( elemento => {
+                          return(
+                              <option key={elemento.idSala} value={elemento.idSala}>
+                                  {elemento.andar}
+                              </option>
+                          );
+                      } )
+                    }
                 </select>
 
                 <input 
                   required
                   type="text"
-                  // name=""
-                  // value={this.state.}
+                  name="NomeSalaNova"
+                  value={this.state.NomeSalaNova}
                   onChange={this.atualizaStateCampo}
                   placeholder="Nome"
                 />
@@ -90,8 +169,8 @@ export default class Home extends Component{
                 <input 
                     required
                     type="text"
-                    // name=""
-                    // value={this.state.}
+                    name="MetragemSalaNova"
+                    value={this.state.MetragemSalaNova}
                     onChange={this.atualizaStateCampo}
                     placeholder="Metragem..."
                 />
@@ -102,22 +181,46 @@ export default class Home extends Component{
             </form>
           </div>
           <div>
+            <h3>Cadastrar Equipamento</h3>
             <form>
               <div>
 
-                <select>
+                <select
+                name='TipoEquipamentoNovo'
+                id='tipoEquip'
+                onChange={this.atualizaStateCampo}
+                value={this.state.TipoEquipamentoNovo} >
+
+                  <option value="0" disabled > Selecione o Tipo de Equipamento </option>
+                  {
+                    this.state.listaTiposEquipamentos.map( elemento => {
+                        return(
+                            <option key={elemento.idTipoEquipamento} value={elemento.idTipoEquipamento}>
+                                {elemento.nomeEquipamento}
+                            </option>
+                        );
+                    } )
+                  }
 
                 </select>
 
-                <select>
+                <select
+                  name='SituacaoNova'
+                  id='andar'
+                  onChange={this.atualizaStateCampo}
+                  value={this.state.SituacaoNova} >
 
+                    <option value="5" disabled > Selecione a Situação </option>
+                    <option value='1' >Ativo</option>
+                    <option value='0' >Inativo</option>
+                    
                 </select>
 
                 <input 
                   required
                   type="text"
-                  // name=""
-                  // value={this.state.}
+                  name="NumeroSerieNovo"
+                  value={this.state.NumeroSerieNovo}
                   onChange={this.atualizaStateCampo}
                   placeholder="Número de Série"
                 />
@@ -125,8 +228,8 @@ export default class Home extends Component{
                 <input 
                   required
                   type="text"
-                  // name=""
-                  // value={this.state.}
+                  name="NumeroPatrimonioNovo"
+                  value={this.state.NumeroPatrimonioNovo}
                   onChange={this.atualizaStateCampo}
                   placeholder="Número de Patrimônio"
                 />
@@ -134,8 +237,8 @@ export default class Home extends Component{
                 <input 
                   required
                   type="text"
-                  // name=""
-                  // value={this.state.}
+                  name="DescricaoNova"
+                  value={this.state.DescricaoNova}
                   onChange={this.atualizaStateCampo}
                   placeholder="Descrição..."
                 />
